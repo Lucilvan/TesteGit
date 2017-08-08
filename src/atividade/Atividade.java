25eb008c18fb22c4383ba2f63a5d7a1491431420
i2284 <i2284@02LAB209APC01.rn.senac.br>
2017-08-08 19:01:38 -0300

    Testet

--- src/atividade/Atividade.java
+++ src/atividade/Atividade.java
@@ -5,28 +5,5 @@ import javax.swing.JOptionPane;
 public class Atividade {
             //sdgs
     public static void main(String[] args) {
-        int opcao, qtdMeia = 0, qtdInteiro = 0;
-        int teste; int teste1; int teste2; int teste3;   
-        do{
-            opcao = Integer.parseInt(JOptionPane.showInputDialog(":::Cinema Senac:::\n"
-                    + "Qual o tipo de bilhete deseja\n"
-                    + "1 - Inteira \n 2 - Meio \n 0 - Sair \n Escolha uma das opçoes acima:"));
-            switch(opcao){
-                case 1:
-                    qtdInteiro++;
-                    break;
-                case 2:
-                    qtdMeia++;
-                    break;
-                case 0:
-                    JOptionPane.showMessageDialog(null, ":::Bilhetes Vendido:::\n"
-                            + "Inteiro: "+qtdInteiro+"\n Meia: "+qtdMeia);
-                    break;
-                default:
-                    JOptionPane.showMessageDialog(null, "Escolha uma opção válida!");
-                    break;
-            }
-        }while(opcao!=0 || ((qtdInteiro+qtdMeia)==30));
     }
-    
 }
